882024e76a55f635f9c26809b78c30d4661623d8
mac_imc700 <mac_imc700@163.com>
2021-04-26 17:02:15 +0800

    在mac运行起来,修改网关路由设置

--- ks_eureka/src/test/java/com/ks/ks_eureka/KsEurekaApplicationTests.java
+++ ks_eureka/src/test/java/com/ks/ks_eureka/KsEurekaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KsEurekaApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
